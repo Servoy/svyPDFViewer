b3af33df8b1a6e5b1d5569362317db545eb91d74
Sean <sdevlin@servoy.com>
2016-11-16 13:21:35 -0500

    Updates to the sample solution
    for spec changes in prior commit

--- svyPDFViewerExample/forms/pdfViewer.js
+++ svyPDFViewerExample/forms/pdfViewer.js
@@ -22,19 +22,30 @@ var documentURL = 'http://www.cbu.edu.zm/downloads/pdf-sample.pdf';
  *
  * @properties={typeid:24,uuid:"FAAFF2F3-46FE-4FD6-9C4B-55EF13903824"}
  */
-function onDataChange(oldValue, newValue, event) {
-	elements.pdfViewer.documentURL = documentURL;
+function onDataChangeDocumentURL(oldValue, newValue, event) {
+	loadDocument();
 	return true
 }
 
 /**
- * Reloads the document. 
- * This may be needed when the document has changed on the back-end, but is still cached in the viewer.
- *
- * @param {JSEvent} event the event that triggered the action
+ * (Re)Loads the document
+ * @private 
  *
  * @properties={typeid:24,uuid:"F7E79E64-D024-433B-A32A-1934DE5CB699"}
  */
-function reload(event) {
-	elements.pdfViewer.reload();
+function loadDocument() {
+	elements.pdfViewer.loadDocument(documentURL);
+}
+
+/**
+ * Load document when form is loaded
+ *
+ * @param {JSEvent} event the event that triggered the action
+ *
+ * @private
+ *
+ * @properties={typeid:24,uuid:"B2E29DB1-646D-49E1-AB55-DF1FCFDD03F4"}
+ */
+function onLoad(event) {
+	loadDocument();
 }
