7520ca46d6703935efd4847112689b18ab64a6fa
R. van Veen <rvanveen@servoy.com>
2020-05-01 15:13:37 +0200

    SVY-14952 * Updated sample solution

--- svyPDFViewerExample/forms/pdfViewer.js
+++ svyPDFViewerExample/forms/pdfViewer.js
@@ -1,11 +1,4 @@
 /**
- * @type {Number}
- *
- * @properties={typeid:35,uuid:"8355F6CB-ACDB-419D-A503-F2AC4B88C86C",variableType:4}
- */
-var pageNumber = null;
-
-/**
  * @properties={typeid:35,uuid:"100AFDEC-AD11-4EC1-B4C0-9EBD6CB69FA1",variableType:-4}
  */
 var byteArray
@@ -29,19 +22,9 @@ var documentURL = 'https://servoy.com/download/reflecta-case-study/?wpdmdl=4295'
 function loadDocument() {
 	//Download the remote file
 	var file = plugins.http.getMediaData(documentURL);
-	
-	//Write the file to a remote file
-	var remoteFileName = application.getUUID().toString() + '.pdf';
-	var remoteFile = plugins.file.convertToRemoteJSFile('/'+remoteFileName)
-	remoteFile.setBytes(file,true);
-	
-	//Convert the remote file to a url, and display it in the PDF viewer
-	var remoteUrl = plugins.file.getUrlForRemoteFile('/'+remoteFileName);
-	if(remoteUrl) {
-		byteArray = null;
-		elements.pdfViewer.documentURL = remoteUrl;
+	if(file) {
+		byteArray = file;
 	}
-
 }
 
 /**
@@ -67,22 +50,7 @@ function onShow(firstShow, event) {
  *
  * @properties={typeid:24,uuid:"B482AE1C-AE84-4FE6-9619-35A25B485808"}
  */
-function loadDocumentDataprovider(event) {
-	byteArray = solutionModel.getMedia('test.pdf').bytes;
-}
-
-/**
- * @param oldValue
- * @param newValue
- * @param {JSEvent} event
- *
- * @return {boolean}
- *
- * @protected
- *
- * @properties={typeid:24,uuid:"43E911F6-C403-4410-A4D2-55571358752A"}
- */
-function onDataChange(oldValue, newValue, event) {
-	elements.pdfViewer.pageNumber = newValue;
-	return false;
+function resetToDefault(event) {
+	documentURL = 'https://servoy.com/download/reflecta-case-study/?wpdmdl=4295';
+	loadDocument()
 }
