cae0134654adf344e6417a1a38f4c209cd8a51fa
R. van Veen <rvanveen@servoy.com>
2020-05-01 15:06:03 +0200

    Removed default sample pdf from viewer

--- pdfviewer/pdfJsViewer/web/viewer.js
+++ pdfviewer/pdfJsViewer/web/viewer.js
@@ -4246,7 +4246,7 @@ var defaultOptions = {
     kind: OptionKind.VIEWER + OptionKind.PREFERENCE
   },
   defaultUrl: {
-    value: 'compressed.tracemonkey-pldi-09.pdf',
+    value: '',
     kind: OptionKind.VIEWER
   },
   defaultZoomValue: {
