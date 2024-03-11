3a4290801c9133fd97dd953bcabf681fa7aba9d4
lvostinar <lvostinar@servoy.com>
2024-03-11 13:57:50 +0200

    SVY-18717 pdfJsViewer: setToolbarControlsVisibility() doesn't work

--- pdfviewer/pdfJsViewer/pdfJsViewer_server.js
+++ pdfviewer/pdfJsViewer/pdfJsViewer_server.js
@@ -4,14 +4,14 @@ $scope.api.setFieldValues = function(values) {
 
 $scope.api.setToolbarControlsVisibility = function(ids, visible) {
     const obj = {};
-    if (ids) {
-        for (let i = 0; i< ids.length;i++) {
-            obj[ids[i]] = visible;
-        }
-    }
     if ($scope.model.toolbarControlsVisibility){
         for (let id in $scope.model.toolbarControlsVisibility) {
             obj[id] = $scope.model.toolbarControlsVisibility[id];
+        }
+    }
+    if (ids) {
+        for (let i = 0; i< ids.length;i++) {
+            obj[ids[i]] = visible;
         }
     }
     $scope.model.toolbarControlsVisibility = obj;
@@ -19,14 +19,14 @@ $scope.api.setToolbarControlsVisibility = function(ids, visible) {
 
 $scope.api.setFieldControlsVisibility = function(ids, visible) {
     const obj = {};
-    if (ids) {
-        for (let i = 0; i< ids.length;i++) {
-            obj[ids[i]] = visible;
-        }
-    }
     if ($scope.model.fieldControlsVisibility){
         for (let id in $scope.model.fieldControlsVisibility) {
             obj[id] = $scope.model.fieldControlsVisibility[id];
+        }
+    }
+    if (ids) {
+        for (let i = 0; i< ids.length;i++) {
+            obj[ids[i]] = visible;
         }
     }
     $scope.model.fieldControlsVisibility = obj;
