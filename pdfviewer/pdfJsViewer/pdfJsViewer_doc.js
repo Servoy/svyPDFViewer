3bb4dc53c23abb9b66aae1124c9ee01dda838be4
cPecican <cpecican@servoy.com>
2024-11-27 17:22:21 +0200

    SVY-19737 create docs for the pdf(js)viewer - update

--- pdfviewer/pdfJsViewer/pdfJsViewer_doc.js
+++ pdfviewer/pdfJsViewer/pdfJsViewer_doc.js
@@ -1,6 +1,62 @@
+/**
+ * pdfJsViewer provides more control and customization options than pdfViewer.
+ * It is more flexible and can handle advanced features like drawing and digital signatures.
+ * If customization is not required, consider using pdfViewer.
+ **/
+
 
 /**
- *  reload the pdf document
+ *  forces a reload of the current url
  **/
 function reload () {
+}
+
+/**
+ * Returns a map containing the field values.
+ * 
+ * @returns {Map} A map of field values.
+ **/
+function getFieldValues() {
+}
+
+/**
+ * Sets the field values.
+ *
+ * @param {Map} values - A map containing the field values to be set.
+ **/
+function setFieldValues(values) {
+}
+
+/**
+ * Returns an array of strings representing the field names.
+ * 
+ * @returns {String[]} An array of field names.
+ **/
+function getFieldNames() {
+}
+
+/**
+ * Returns an array of strings representing the toolbar control IDs.
+ * 
+ * @returns {String[]} An array of toolbar control IDs.
+ **/
+function getToolbarControlIds() {
+}
+
+/**
+ * Sets the visibility of the toolbar controls.
+ *
+ * @param {String[]} ids - An array of toolbar control IDs.
+ * @param {Boolean} visible - A boolean indicating the visibility of the toolbar controls.
+ **/
+function setToolbarControlsVisibility(ids, visible) {
+}
+
+/**
+ * Sets the visibility of the field controls.
+ *
+ * @param {String[]} names - An array of field control names.
+ * @param {Boolean} visible - A boolean indicating the visibility of the field controls.
+ **/
+function setFieldControlsVisibility(names, visible) {
 }
--- pdfviewer/pdfViewer/pdfViewer_doc.js
+++ pdfviewer/pdfViewer/pdfViewer_doc.js
@@ -1,6 +1,11 @@
+/**
+ * pdfViewer uses the PDF viewing capabilities built into the browser.
+ * It is simple and relies on tools already available in the client's browser, offering limited personalization.
+ * For more customization options, consider using pdfJsViewer.
+ */
 
 /**
- *  reload the pdf document
+ *  forces a reload of the current url
  **/
 function reload () {
 }
