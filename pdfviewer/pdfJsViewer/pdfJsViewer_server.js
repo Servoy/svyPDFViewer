86378241ecbb48e9714590003af16e39e8961a49
lvostinar <lvostinar@servoy.com>
2026-02-12 10:54:17 +0200

    SVY-20903 pdfjsviewer rhino error (temporary fix for this issue, until we change rhino)

--- pdfviewer/pdfJsViewer/pdfJsViewer_server.js
+++ pdfviewer/pdfJsViewer/pdfJsViewer_server.js
@@ -5,12 +5,12 @@ $scope.api.setFieldValues = function(values) {
 $scope.api.setToolbarControlsVisibility = function(ids, visible) {
     const obj = {};
     if ($scope.model.toolbarControlsVisibility){
-        for (let id in $scope.model.toolbarControlsVisibility) {
+        for (var id in $scope.model.toolbarControlsVisibility) {
             obj[id] = $scope.model.toolbarControlsVisibility[id];
         }
     }
     if (ids) {
-        for (let i = 0; i< ids.length;i++) {
+        for (var i = 0; i< ids.length;i++) {
             obj[ids[i]] = visible;
         }
     }
@@ -20,12 +20,12 @@ $scope.api.setToolbarControlsVisibility = function(ids, visible) {
 $scope.api.setFieldControlsVisibility = function(ids, visible) {
     const obj = {};
     if ($scope.model.fieldControlsVisibility){
-        for (let id in $scope.model.fieldControlsVisibility) {
+        for (var id in $scope.model.fieldControlsVisibility) {
             obj[id] = $scope.model.fieldControlsVisibility[id];
         }
     }
     if (ids) {
-        for (let i = 0; i< ids.length;i++) {
+        for (var i = 0; i< ids.length;i++) {
             obj[ids[i]] = visible;
         }
     }
